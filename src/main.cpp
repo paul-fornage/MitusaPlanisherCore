6241fca4aa461d155bef4d03de7e6ca27aac07f6
paul fornage <paul@fornage.net>
2025-04-21 12:39:25 -0700

    Added version for use without HMI

--- src/main.cpp
+++ src/main.cpp
@@ -7,40 +7,27 @@
   This code is licensed under the BSD New License. See LICENSE.txt for more info.
 */
 
-#include <Ethernet.h>       // Ethernet library v2 is required
+#include <Arduino.h>
 
-#include <ModbusAPI.h>
-#include <ModbusTCPTemplate.h>
 #include "ClearCore.h"
 
-#include "RegisterDefinitions.h"
 
-// ModBus TCP stuff
-class ModbusEthernet : public ModbusAPI<ModbusTCPTemplate<EthernetServer, EthernetClient>> {};
-const IPAddress remote(192, 168, 0, 211);  // Address of Modbus Slave device
-byte mac[] = { 0x24, 0x15, 0x10, 0xB0, 0x45, 0xA4 }; // MAC address is ignored but because of C++ types, you still need to give it garbage
-IPAddress ip(192, 168, 0, 178); // The IP address will be dependent on your local network
-ModbusEthernet mb;               // Declare ModbusTCP instance
-
-bool is_HMI_comm_good = false;
-
-bool is_mandrel_latch_closed;           // COIL R   Mandrel latch sensor reading. True for closed and safe
-bool is_fingers_down;                   // COIL R   Are the workpiece holding fingers commanded down
-bool is_homed;                          // COIL R   Has the axis been homed
-bool is_fault;                          // COIL R   Software fault detection. Indicates an unexpected condition
-bool is_ready_for_cycle;                // COIL R   Is the clearcore ready to execute its program
-volatile bool is_e_stop;                // COIL R   Is the Emergency Stop currently active
-bool is_job_active;                     // COIL R   Is the clearcore executing its program
-bool is_ready_for_manual_control;       // COIL R   Is the clearcore ready for manual control commands
-bool is_roller_down;                    // COIL R   Is the roller down/engaged
-bool is_commanded_pos;                  // COIL R/W Has the HMI requested a new commanded position?
-bool is_rth_button_latched;             // COIL R/W 'Return to home' button latch state
-bool is_axis_homing_button_latched;     // COIL R/W 'Run axis homing sequence' button latch state
-bool is_set_job_start_button_latched;   // COIL R/W 'Set start to current position' button latch state
-bool is_set_job_end_button_latched;     // COIL R/W 'Set end to current position' button latch state
-bool is_set_job_park_button_latched;    // COIL R/W 'Set park to current position' button latch state
-bool commanded_fingers;                 // COIL R/W Finger state commanded by the HMI, true means engaged, false means disengaged
-bool commanded_roller;                  // COIL R/W Roller state commanded by the HMI, true means engaged, false means disengaged
+bool is_mandrel_latch_closed;           // Mandrel latch sensor reading. True for closed and safe
+bool is_fingers_down;                   // Are the workpiece holding fingers commanded down
+bool is_homed;                          // Has the axis been homed
+bool is_fault;                          // Software fault detection. Indicates an unexpected condition
+bool is_ready_for_cycle;                // Is the clearcore ready to execute its program
+volatile bool is_e_stop;                // Is the Emergency Stop currently active
+bool is_job_active;                     // Is the clearcore executing its program
+bool is_ready_for_manual_control;       // Is the clearcore ready for manual control commands
+bool is_roller_down;                    // Is the roller down/engaged
+bool is_commanded_pos;                  // Has the HMI requested a new commanded position?
+bool is_axis_homing_button_latched;     // 'Run axis homing sequence' button latch state
+bool is_set_job_start_button_latched;   // 'Set start to current position' button latch state
+bool is_set_job_end_button_latched;     // 'Set end to current position' button latch state
+bool is_set_job_park_button_latched;    // 'Set park to current position' button latch state
+bool commanded_fingers;                 // Finger state commanded by the HMI, true means engaged, false means disengaged
+bool commanded_roller;                  // Roller state commanded by the HMI, true means engaged, false means disengaged
 
 
 uint16_t actual_position;         // Hreg R   actual position of the axis measured in hundredths of an inch
@@ -57,6 +44,16 @@ uint16_t planish_speed;           // Hreg R/W target speed during engaged portio
 uint16_t fault_code;              // Hreg R   fault code; 0 indicates normal operation
 uint16_t heartbeat_in;            // Hreg W   Contains an arbitrary value set by the HMI
 uint16_t heartbeat_out;           // Hreg R   This should always be equal to `(heartbeat_in*2)%65536`
+
+
+enum PlanishStates {
+  post,
+  homing,
+  idle,
+  manual_jog,
+  e_stop,
+
+};
 
 
 #define ESTOP_SW ConnectorA12 /// These should match \/
@@ -88,7 +85,6 @@ void setup() {
 
   ESTOP_SW.InterruptHandlerSet(e_stop_handler);
 
-  Ethernet.begin(mac, ip);
 
   // TODO: https://teknic-inc.github.io/ClearCore-library/_move_position_absolute_8cpp-example.html
 
@@ -103,27 +99,9 @@ void setup() {
 
   CARRIAGE_MOTOR.VelMax(CARRIAGE_MOTOR_MAX_VEL);
   CARRIAGE_MOTOR.AccelMax(CARRIAGE_MOTOR_MAX_ACCEL);
-
-
-  // Make sure the physical link is up before continuing.
-  while (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Waiting for Ethernet connection...");
-    delay(100);
-  }
-  mb.client();
-  while (!mb.isConnected(remote)) {
-    mb.connect(remote);
-    delay(500);
-    Serial.println("Waiting for Modbus connection...");
-  }
-  is_HMI_comm_good = true;
 }
 
-bool coil = false;
-uint16_t reg1 = 0;
-uint16_t reg2 = 0;
-uint32_t showLast = 0;
-uint16_t sum = 0;
+
 
 void loop() {
   if (is_e_stop) {
@@ -131,25 +109,9 @@ void loop() {
   } else {
     Serial.println("Normal Operation");
   }
-  if (mb.isConnected(remote)) {   // Check if connection to Modbus Slave is established
-    mb.readCoil(remote, 100, &coil);  // Initiate Read Hreg from Modbus Slave
-    Serial.print("coil 100: ");
-    Serial.println(coil ? "True" : "False");
-    mb.readHreg(remote, 16, &reg1);  // Initiate Read Hreg from Modbus Slave
-    Serial.print("reg 16: ");
-    Serial.println(reg1);
-    mb.readHreg(remote, 17, &reg2);  // Initiate Read Hreg from Modbus Slave
-    Serial.print("reg 17: ");
-    Serial.println(reg2);
-    sum = reg1+reg2;
-    mb.writeHreg(remote, 18, &sum);  // Initiate Read Hreg from Modbus Slave
 
-  } else {
-    is_HMI_comm_good = false;
-    mb.connect(remote);           // Try to connect if not connected
-  }
-  delay(10);                     // Pulling interval
-  mb.task();                      // Common local Modbus task
+  delay(10);
+
 }
 
 
@@ -168,53 +130,7 @@ void e_stop_handler() {
   Serial.println("Emergency Stop");
 }
 
-/**
- * Reads all coils that can be changed by the HMI
- * @return true for success
- */
-bool read_coils() {
-  uint8_t remaining_tries = HMI_CONNECTION_TRIES_BEFORE_ERROR;
-  while (!mb.isConnected(remote) && remaining_tries > 0) {
-    mb.connect(remote);
-    remaining_tries--;
-  }
-  if (remaining_tries == 0) {
-    return false;
-  }
 
-  mb.readCoil(remote, IS_COMMANDED_POS_COIL_ADDR, &is_commanded_pos);
-  mb.readCoil(remote, IS_RTH_BUTTON_LATCHED_COIL_ADDR, &is_rth_button_latched);
-  mb.readCoil(remote, IS_AXIS_HOMING_BUTTON_LATCHED_COIL_ADDR, &is_axis_homing_button_latched);
-  mb.readCoil(remote, IS_SET_JOB_START_BUTTON_LATCHED_COIL_ADDR, &is_set_job_start_button_latched);
-  mb.readCoil(remote, IS_SET_JOB_END_BUTTON_LATCHED_COIL_ADDR, &is_set_job_end_button_latched);
-  mb.readCoil(remote, IS_SET_JOB_PARK_BUTTON_LATCHED_COIL_ADDR, &is_set_job_park_button_latched);
-  mb.readCoil(remote, COMMANDED_FINGERS_COIL_ADDR, &commanded_fingers);
-  mb.readCoil(remote, COMMANDED_ROLLER_COIL_ADDR, &commanded_roller);
-
-  return true;
-}
-
-/**
- * Reads all registers that can be set by HMI
- * @return True for success
- */
-bool read_registers() {
-  uint8_t remaining_tries = HMI_CONNECTION_TRIES_BEFORE_ERROR;
-  while (!mb.isConnected(remote) && remaining_tries > 0) {
-    mb.connect(remote);
-    remaining_tries--;
-  }
-  if (remaining_tries == 0) {
-    return false;
-  }
-  mb.readHreg(remote, COMMANDED_POSITION_REG_ADDR, &commanded_position);
-  mb.readHreg(remote, JOG_SPEED_REG_ADDR, &jog_speed);
-  mb.readHreg(remote, PLANISH_SPEED_REG_ADDR, &planish_speed);
-  mb.readHreg(remote, HEARTBEAT_IN_REG_ADDR, &heartbeat_in);
-  heartbeat_out = (heartbeat_in*2)%65536;
-  mb.writeHreg(remote, HEARTBEAT_OUT_REG_ADDR, &heartbeat_out);
-  return true;
-}
 
 
 bool move_to_position(uint16_t new_position) {
@@ -229,7 +145,6 @@ bool move_to_position(uint16_t new_position) {
   commanded_position = new_position;
   if(motor_movement_checks()) {
     CARRIAGE_MOTOR.Move(commanded_position, StepGenerator::MOVE_TARGET_ABSOLUTE);
-    // TODO: Move() returns a bool and docs don't say why. use it if it's a failure flag
   }
 }
 
